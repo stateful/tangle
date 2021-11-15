e9b3afa4e0c9872a089c341f67b672a271f26e7b
Christian Bromann <git@bromann.dev>
2021-11-16 00:53:04 +0100

    Setup test pipeline (#9)

--- src/vrx.spec.ts
+++
@@ -1,8 +0,0 @@
-import Vrx = require("./vrx");
-
-describe("Vrx.Bus", () => {
-  it("should instiantiate a bus instance", () => {
-    const bus = new Vrx.Bus<object>("testing", {}, []);
-    expect(bus).toBeInstanceOf(Vrx.Bus);
-  });
-});
