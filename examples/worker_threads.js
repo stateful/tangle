5db5861e083af517e7b3d974d985e1688be35166
Christian Bromann <git@bromann.dev>
2021-11-17 12:20:22 +0100

    extend example

--- examples/worker_threads.js
+++ examples/worker_threads.js
@@ -12,24 +12,36 @@ if (isMainThread) {
         new Worker(filename, { workerData: { id: 'worker #2' } }),
         new Worker(filename, { workerData: { id: 'worker #3' } })
     ]).subscribe((bus) => {
-        bus.listen('onCustomEvent', (msg) => {
-            console.log('Received from worker thread:', msg);
-            bus.broadcast({ onCustomWorkerEvent: 'worker #3' });
-        });
+        bus.listen('onCustomEvent', (msg) =>
+            console.log('Received from worker thread:', msg));
 
         bus.listen('onExit', () => {
             console.log('Bye bye');
             ch.providers.map((p) => p.terminate());
         });
+
+        setTimeout(() => bus.broadcast({ onCustomWorkerEvent: 'worker #3' }), 100);
     });
 } else {
     const client = ch.attach('test', {});
 
+    /**
+     * listen to events within the same sandbox
+     */
+    client.listen('onCustomEvent', (msg) =>
+        console.log(`Another worker message received in ${workerData.id}; ${msg}`));
+
+    /**
+     * broadcast to all
+     */
     client.broadcast({ onCustomEvent: `Hello from ${workerData.id} 👋` });
+
+    /**
+     * listen to messages from message bus
+     */
     client.listen('onCustomWorkerEvent', (id) => {
         if (workerData.id === id) {
             client.broadcast({ 'onExit': workerData.id });
-            process.exit(0);
         }
     });
 }
