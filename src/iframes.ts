db49fcdda990ea941ba2e766c04b19226a480f35
Christian Bromann <git@bromann.dev>
2021-11-18 14:25:30 +0100

    add unit tests

--- src/iframes.ts
+++ src/iframes.ts
@@ -11,6 +11,17 @@ import BaseChannel from './channel';
 export default class Channel<T> extends BaseChannel<HTMLIFrameElement, T> {
     public providers: HTMLIFrameElement[] = [];
 
+    constructor (
+        namespace: string,
+        defaultValue: T,
+        /**
+         * for testing purposes
+         */
+        private _window = window
+    ) {
+        super(namespace, defaultValue);
+    }
+
     register (providers: HTMLIFrameElement[]) {
         this.providers.push(...providers);
         return of(...providers).pipe(
@@ -18,13 +29,13 @@ export default class Channel<T> extends BaseChannel<HTMLIFrameElement, T> {
             toArray(),
             map((ps: HTMLIFrameElement[]) => ps.map((p) => (<Provider>{
                 onMessage: (listener) => {
-                    window.onmessage = (ev) => listener(ev.data);
+                    this._window.onmessage = (ev) => listener(ev.data);
                 },
                 postMessage: (message) => {
                     if (!p.contentWindow) {
                         throw new Error('No content window found');
                     }
-                    p.contentWindow.postMessage(message);
+                    p.contentWindow.postMessage(message, '*');
                     return Promise.resolve();
                 },
             }))),
@@ -35,13 +46,13 @@ export default class Channel<T> extends BaseChannel<HTMLIFrameElement, T> {
     attach () {
         return this._initiateClient(<Provider>{
             onMessage: (listener: EventListener) => {
-                window.onmessage = (ev) => listener(ev.data);
+                this._window.onmessage = (ev) => listener(ev.data);
             },
             postMessage: (message: any) => {
-                if (!window.top) {
+                if (!this._window.top) {
                     throw new Error('No top window found!');
                 }
-                window.top.postMessage(message);
+                this._window.top.postMessage(message, '*');
                 return Promise.resolve();
             }
         });
